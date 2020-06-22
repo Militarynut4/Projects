a86dae8dae17a301018710974c82ada546226a73
AndreiRzv <61976709+Militarynut4@users.noreply.github.com>
2020-06-22 11:04:09 +0300

    Add files via upload
    Add or Get your passwords!
    
    Get a random password and store it inside a database!

---
+++ Passwords/main.py
@@ -0,0 +1,55 @@
+import json, string, random, time
+
+print("1. Add password\n2. Get password")
+
+def rChar(x):
+    return "".join(random.choice(string.ascii_letters + string.digits) for x in range(x))
+
+while True:
+    with open("passwords.json", "r") as f:
+        passwords = json.load(f)
+
+    choice = input("?: ")
+    
+    site = input("\nSite name: ")
+
+    if choice == '1':
+
+        if site in passwords:
+
+            print(f"\nThe site already exist!")
+            msg = input("Do you want to change it? Y/N\n")
+            passwords.pop(str(site.lower()))
+            x = 0
+        
+        else:
+            x = 1
+            
+
+        if msg.lower() == "y" or x == 1:
+
+            print(f"\npass: {rChar(18)}")
+
+            passwords[str(site.lower())] = rChar(18)
+
+            with open("passwords.json", "w") as f:
+                json.dump(passwords, f, indent=4)
+            
+            time.sleep(2.4)
+
+    elif choice == '2':
+        
+        if site in passwords:
+
+            print("\n password: " + passwords[site.lower()])
+        
+        else:
+            print("\nThis site doesn't exist in database!")
+        
+        time.sleep(2.4)
+
+
+    else:
+        print("Error: Invalide characters")
+
+    print("\n1. Add password\n2. Get password")
